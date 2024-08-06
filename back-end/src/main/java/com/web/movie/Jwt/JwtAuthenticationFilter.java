f1c12d3c81e8fcf8fbbb10df874774cee5c9f0bc
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-08-06 16:33:40 +0700

    Add 2 class to check jwt of request

---
+++ back-end/src/main/java/com/web/movie/Jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.web.movie.Jwt;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Autowired private UserDetailsService userDetailsService;
+    @Autowired private JwtTokenProvider jwtTokenProvider;
+    
+    @Override
+    protected void doFilterInternal(@SuppressWarnings("null") HttpServletRequest request, @SuppressWarnings("null") HttpServletResponse response,FilterChain filterChain) throws ServletException, IOException{
+        try{
+            String jwt = getJwtFromRequest(request);
+            if(jwtTokenProvider.validateToken(jwt)){
+                String username = jwtTokenProvider.getUserNameFromJWT(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if(userDetails!=null){
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Faild on set user authentication");
+        }
+        filterChain.doFilter(request, response);
+    }
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        // Kiểm tra xem header Authorization có chứa thông tin jwt không
+        if (bearerToken!=null &&bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
