103d0e643db296a2df61153ceb7ca046dbce04f2
Phuong22122003 <hnguyenphuong09@gmail.com>
2024-08-06 16:41:06 +0700

    fix id=1 to id = movieId param

--- back-end/src/main/java/com/web/movie/Repository/MovieGenreRepository.java
+++ back-end/src/main/java/com/web/movie/Repository/MovieGenreRepository.java
@@ -8,6 +8,6 @@ import com.web.movie.Entity.MovieGenre;
 import com.web.movie.Entity.MovieGenreKey;
 
 public interface MovieGenreRepository extends JpaRepository<MovieGenre,MovieGenreKey>{
-    @Query(value = "select top(1) GENRE_ID from MOVIE_GENRE where MOVIE_ID = 1 order by newId()",nativeQuery = true)
+    @Query(value = "select top(1) GENRE_ID from MOVIE_GENRE where MOVIE_ID = :movieId  order by newId()",nativeQuery = true)
     public Integer findGenre(@Param("movieId") Integer moviedId);
 }
